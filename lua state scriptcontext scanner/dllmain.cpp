b61bc8d92393885c2f1d715201f5619fd41c34b1
clxyify <98536033+clxyify@users.noreply.github.com>
2022-07-01 18:42:52 -0400

    Update dllmain.cpp

--- lua state scriptcontext scanner/dllmain.cpp
+++ lua state scriptcontext scanner/dllmain.cpp
@@ -11,11 +11,15 @@
    quick reminder: lua_state has not changed since november of 2021.
    scriptcontext can be found in class informer -> RBX::ScriptContext
    most likely the 3rd string from the bottom and split a x between 0.
+   
+   pls dont actually skid off this and cred asdf/clxyify if you use it.
+   NOTE: not all 100% of functions will be run anything freezing is your fault.
 */
 #define x(x) (x - 0x400000 + (DWORD)GetModuleHandleA(0)) /* aslr */
 std::uintptr_t rL; /* used for using functions */
 DWORD ScriptContext; /* scriptcontext holder */
 DWORD ScriptContextVFT = x(0x343C92C + (DWORD)GetModuleHandleA(0)); /* replace with scriptcontextVFTable */
+/* updated for version-cb81695a34b340de */
 
 namespace Memory { /* scanning shit */
 	bool Compare(const char* pData, const char* bMask, const char* szMask)
@@ -59,7 +63,7 @@ namespace Memory { /* scanning shit */
 	}
 }
 
-int create_pipe()
+int create_pipe() // not required i still did it pls create a thread dont use the actual function unless your a retard
 {
 	HANDLE pipe = CreateFileW(TEXT("\\\\.\\pipe\\Script"), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 	ConnectNamedPipe(pipe, NULL);
@@ -68,9 +72,10 @@ int create_pipe()
 	WriteFile(pipe, message.c_str(), message.length(), &numWritten, NULL);
 	return 0;
 } 
+
 std::uint32_t rbx_gettop(std::uintptr_t rl) //thanks fishy
 {
-	return *reinterpret_cast<std::uint32_t*>(rl + 0x14) - *reinterpret_cast<std::uint32_t*>(rl + 0x8) >> 4;
+	return *reinterpret_cast<std::uint32_t*>(rl + 0x14) - *reinterpret_cast<std::uint32_t*>(rl + 0x8) >> 4; //top/base offset and >>4 not changed since 2014?
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
@@ -84,13 +89,14 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 		MessageBoxA(NULL, "Scanning ScriptContext VFTable!!!\nInject twice if it too fast!", "Scanning", MB_TOPMOST);
 		ScriptContext = Memory::ScanForContext(); /* scans VFTable */
 		MessageBoxA(NULL, "Creating rL state thread.", "Threading", MB_TOPMOST);
-        rL = *(std::uintptr_t*)(ScriptContext + 308) - (ScriptContext + 308); /* opperander to scan lua state */
+                rL = *(std::uintptr_t*)(ScriptContext + 308) - (ScriptContext + 308); /* opperander to scan lua state */
 		MessageBoxA(NULL, std::to_string(rL).c_str(), "rL", MB_TOPMOST); /* show rL */
 		create_pipe(); /* create pipe just in case btw because no advanced shit */
 		MessageBoxA(NULL, "Scanning complete! Try gettop?", "Completed", MB_TOPMOST);
-		rbx_gettop(rL);
+		rbx_gettop(rL); //testing purposes
 		MessageBoxA(NULL, "Completed everything, detached.", "Completed", MB_TOPMOST);
     case DLL_THREAD_ATTACH:
+    if (!rbx_gettop) { exit(-1); }// gonna keep this here for the skids
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH: 
         break;
